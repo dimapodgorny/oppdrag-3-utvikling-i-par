2370fc6bd45ad5b29a65e0d23eb845967019ac38
Haavard Bratlie <bratliehaavard@gmail.com>
2025-03-21 10:59:56 +0100

    fix
    forrige commit mangler disse functionene.

--- app.py
+++ app.py
@@ -70,6 +70,42 @@ def get_user_highscore_results(user_id):
     conn.close()
     return top_results
 
+def register_user(name, password):
+    if request.method == 'POST':
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+ 
+        try:
+            sql = 'INSERT INTO users (name, password) VALUES (?, ?)'
+            cursor.execute(sql, (name, hashed_password))
+            conn.commit()
+            conn.close()
+            return render_template('account_created.html', name=name)
+       
+        except sqlite3.IntegrityError:
+            return render_template('register.html', error="Brukernavnet er allerede tatt, prøv et annet!")
+ 
+    return render_template('register.html')
+ 
+def login_user(name, password):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+   
+    sql = 'SELECT id, password FROM users WHERE name = ?'
+    cursor.execute(sql, (name,))
+    user = cursor.fetchone()
+   
+    if user and bcrypt.checkpw(password.encode('utf-8'), user[1]):
+        session['name'] = name
+        session['user_id'] = user[0]
+        conn.close()
+        return redirect('/')
+    else:
+        conn.close()
+        return render_template('login.html', error="Feil brukernavn eller passord!")
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
